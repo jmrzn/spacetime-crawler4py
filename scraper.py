d83210b18a57622c0b69945829a4f38a5b82f2ed
Nina Le <ninal11@uci.edu>
2026-04-29 21:31:49 +0000

    Save HTML of each page

--- scraper.py
+++ scraper.py
@@ -1,8 +1,13 @@
 import re
+import os
+import json
+from hashlib import md5
 from urllib.parse import urlparse, urljoin, urldefrag
 from bs4 import BeautifulSoup
 
 MAX_SIZE = 1024 * 1024 * 15 # 15 MB
+HTML_DIR = "pages"
+URL_MAP = "url_map.jsonl"
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -20,6 +25,7 @@ def extract_next_links(url, resp):
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     links = []
 
+    # Check that response is valid and has content
     if not is_valid_resp(resp):
         return links
 
@@ -33,8 +39,11 @@ def extract_next_links(url, resp):
         pass
     
     html = resp.raw_response.content
+    save_html(resp.url, html)
     soup = BeautifulSoup(html, "html.parser")
 
+    # Extract hyperlinks from the HTML <a> tags
+    # Defragment and join partial URLs
     for tag in soup.find_all("a", href=True):
         href = tag.get("href")
         link, _ = urldefrag(urljoin(resp.url, href))
@@ -73,10 +82,28 @@ def is_valid(url):
         raise
 
 def is_valid_resp(resp):
+    # Return True if the response's status is 200 and has content; return False otherwise
     return (resp.status == 200 and
             resp.raw_response and
             resp.raw_response.content and
             len(resp.raw_response.content.strip()) > 0)
+
+def save_html(url, html):
+    # Create HTML directory if it doesn't already exist
+    os.makedirs(HTML_DIR, exist_ok=True)
+    # Hash URL to create a unique file name
+    file_name = md5(url.encode()).hexdigest()
+    # Create file path
+    path = os.path.join(HTML_DIR, file_name + ".html")
+
+    # Save HTML to file
+    with open(path, "w") as f:
+        f.write(html)
+    
+    # Create jsonl file for URL mapping, if it doesn't already exist
+    # Append URL and corresponding path
+    with open(URL_MAP, "a") as m:
+        m.write(json.dumps({"url": url, "path": path}) + "\n")
 
 # if __name__ == "__main__":
 #     class TestRawResponse:
@@ -89,24 +116,28 @@ def is_valid_resp(resp):
 #             self.status = status
 #             self.raw_response = TestRawResponse(content)
     
-    # response1 = TestResponse(url = "https://ics.uci.edu/", status = 200,
-    #     content = """<a href="/research-areas/"</a>""") # Partial url
-    # response2 = TestResponse(url = "https://ics.uci.edu/", status = 200,
-    #     content = """<a href="#a11y-skip-link-content"</a>""") # Fragment only
-    # response3 = TestResponse(url = "https://ics.uci.edu/", status = 200,
-    #     content = """<a href="https://ics.uci.edu/facts-figures/ics-mission-history/"</a>""") # Full url
-    # response4 = TestResponse(url = "https://ics.uci.edu/", status = 400,
-    #     content = """<a href="https://ics.uci.edu/facts-figures/ics-mission-history/"</a>""") # Status is not 200
-    # response5 = TestResponse(url = "https://ics.uci.edu/", status = 200,
-    #     content = "") # Status is 200, but no data
+#     response1 = TestResponse(url = "https://ics.uci.edu/", status = 200,
+#         content = """<a href="/research-areas/"</a>""") # Partial url
+#     response2 = TestResponse(url = "https://ics.uci.edu/", status = 200,
+#         content = """<a href="#a11y-skip-link-content"</a>""") # Fragment only
+#     response3 = TestResponse(url = "https://ics.uci.edu/", status = 200,
+#         content = """<a href="https://ics.uci.edu/facts-figures/ics-mission-history/"</a>""") # Full url
+#     response4 = TestResponse(url = "https://ics.uci.edu/", status = 400,
+#         content = """<a href="https://ics.uci.edu/facts-figures/ics-mission-history/"</a>""") # Status is not 200
+#     response5 = TestResponse(url = "https://ics.uci.edu/", status = 200,
+#         content = "") # Status is 200, but no data
 
-    # links = []
-    # links += extract_next_links("https://ics.uci.edu/", response1) # ['https://ics.uci.edu/research-areas/']
-    # links += extract_next_links("https://ics.uci.edu/", response2) # ['https://ics.uci.edu/']
-    # links += extract_next_links("https://ics.uci.edu/", response3) # ['https://ics.uci.edu/facts-figures/ics-mission-history/']
-    # links += extract_next_links("https://ics.uci.edu/", response4) # []
-    # links += extract_next_links("https://ics.uci.edu/", response5) # []
-    # print(links)
+#     links = []
+#     links += extract_next_links("https://ics.uci.edu/", response1) # ['https://ics.uci.edu/research-areas/']
+#     links += extract_next_links("https://ics.uci.edu/", response2) # ['https://ics.uci.edu/']
+#     links += extract_next_links("https://ics.uci.edu/", response3) # ['https://ics.uci.edu/facts-figures/ics-mission-history/']
+#     links += extract_next_links("https://ics.uci.edu/", response4) # []
+#     links += extract_next_links("https://ics.uci.edu/", response5) # []
+#     print(links)
+
+# save_html("https://ics.uci.edu/", "test 1")
+# save_html("https://ics.uci.edu/facts-figures/ics-mission-history/", "test 2")
+
 # print(is_valid("https://wics.ics.uci.edu/")) # True
 # print(is_valid("https://ics.uci.edu/")) # True
 # print(is_valid("https://www.instagram.com/wicsuci/")) # False
